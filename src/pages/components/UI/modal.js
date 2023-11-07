d1b8c9bb9bc64b6b5660170f043f9b2a06b636d3
RubasznyChomik <poreba.mateusz@gmail.com>
2023-11-07 15:46:33 +0100

    x2

--- src/pages/components/UI/modal.js
+++ src/pages/components/UI/modal.js
@@ -6,12 +6,11 @@ const Backdrop = props => {
   return <div className={styles.backdrop} onClick={props.onClose} />
 };
 
-
 //<div className={styles.content}>{props.children}</div>
 const ModalOverlay = props => {
   return (
     <div className={styles.modal}>
-      <div >{props.children}</div>
+      <div className={styles.content}>{props.children}</div>
     </div>
   )
 };
