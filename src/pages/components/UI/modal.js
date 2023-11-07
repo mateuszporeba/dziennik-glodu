3d146fd55c18c71900419854f8dadbe911c14cd9
RubasznyChomik <poreba.mateusz@gmail.com>
2023-11-07 15:31:33 +0100

    x2

--- src/pages/components/UI/modal.js
+++ src/pages/components/UI/modal.js
@@ -1,15 +1,15 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import classes from './modal.module.css';
+import styles from './modal.module.css';
 
 const Backdrop = props => {
-  return <div className={classes.backdrop} onClick={props.onClose} />
+  return <div className={styles.backdrop} onClick={props.onClose} />
 };
 
 const ModalOverlay = props => {
   return (
-    <div className={classes.modal}>
-      <div className={classes.content}>{props.children}</div>
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
     </div>
   )
 };
